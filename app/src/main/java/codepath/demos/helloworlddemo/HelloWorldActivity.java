82670e768a831ca25cbd8c629e2e40c8016aad65
nigesh248 <53265551+nigesh248@users.noreply.github.com>
2020-04-17 15:32:16 +0530

    Update HelloWorldActivity.java

--- app/src/main/java/codepath/demos/helloworlddemo/HelloWorldActivity.java
+++ app/src/main/java/codepath/demos/helloworlddemo/HelloWorldActivity.java
@@ -15,7 +15,9 @@ public class HelloWorldActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater()inflate(R.menu.activity_hello_world, menu);
+		getMenuInflater().inflate(R.menu.activity_hello_world, menu);
+		int i =1;
+		i =+1;
 		return true;
 	}
 
