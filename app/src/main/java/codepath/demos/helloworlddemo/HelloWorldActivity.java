fb0e1e1cad554a9f386eb462b29af7f88347a983
nigesh248 <53265551+nigesh248@users.noreply.github.com>
2020-05-04 19:56:59 +0530

    Update HelloWorldActivity.java

--- app/src/main/java/codepath/demos/helloworlddemo/HelloWorldActivity.java
+++ app/src/main/java/codepath/demos/helloworlddemo/HelloWorldActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import org.junit.Test;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 
 public class HelloWorldActivity extends Activity {
 
