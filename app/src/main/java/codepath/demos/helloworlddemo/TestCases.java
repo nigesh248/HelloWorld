049feed832f207a1922c033dccda03b20366d41d
nigesh248 <53265551+nigesh248@users.noreply.github.com>
2020-05-06 12:37:20 +0530

    Create TestCases.java

---
+++ app/src/main/java/codepath/demos/helloworlddemo/TestCases.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+    @Test
+    public void sampleUnitTest() {
+
+        assertEquals(0,1);
+    }
