3fec72f9237c8aaaaa2342594f91d43d38fbf959
nigesh248 <53265551+nigesh248@users.noreply.github.com>
2020-05-06 13:04:41 +0530

    Update TestCases.java

--- app/src/main/java/codepath/demos/helloworlddemo/TestCases.java
+++ app/src/main/java/codepath/demos/helloworlddemo/TestCases.java
@@ -2,9 +2,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-
+public class TestCases
+{
     @Test
     public void sampleUnitTest() {
 
         assertEquals(0,1);
     }
+}
